d8ea0567f45e26706476bb8a66158ff3670dc7ad
mccobb <mccobb@ncsu.edu>
2014-12-11 19:33:27 -0500

    I fixes

--- js/sync.js
+++ js/sync.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-    var deg = 0;
-    
-    setInterval(function() {
-        document.body.style.webkitFilter = 'hue-rotate(' + deg + 'deg)';
-        deg += 1;
-        if (deg >= 360) {
-            deg = 0;
-        }
-    }, 100);
 
     var userid = Math.floor((Math.random() * 10000000) + 1);
     console.log('userid: ' + userid);
@@ -41,14 +32,14 @@ $(document).ready(function() {
     function make_event(type, x, y) {
         
         var evt = document.createEvent("MouseEvents");
-        evt.initMouseEvent(type, true, true, window, 1, null, null, x, y, false, false, false, false, 0, null);
+        evt.initMouseEvent(type, true, true, window, 1, null, null, x * window.innerWidth, y * window.innerHeight, false, false, false, false, 0, null);
         evt.data = "fake";
         document.getElementsByTagName("canvas")[0].dispatchEvent(evt);
     }
 
-    window.sendws = function (type, cx, cy) {
+    window.sendws = function (type, x, y) {
         if (ws.readyState == 1) {
-            ws.send(JSON.stringify({'userid': userid, 'type': type, 'cx': cx, 'cy': cy}));
+            ws.send(JSON.stringify({'userid': userid, 'type': type, 'cx': x, 'cy': y}));
         }
     }
     
@@ -64,6 +55,8 @@ $(document).ready(function() {
                 cy = e.clientY;
             }
             
+            
+            
             var remap = {
                 'touchstart': 'mousedown',
                 'touchmove': 'mousemove',
@@ -75,7 +68,7 @@ $(document).ready(function() {
                 type = remap[type];
             }
             
-            sendws(type, cx, cy);
+            sendws(type, cx / window.innerWidth, cy / window.innerHeight);
         }
     }
     
